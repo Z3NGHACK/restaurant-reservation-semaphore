af9b3965d347599b6b38bf56ebee3ebf21b6da6d
Z3NGHACK <zenghack1@gmail.com>
2025-01-07 21:45:40 +0700

    Structure

---
+++ srr/src/main/java/srr/srr/Reservation/ReservationController.java
@@ -0,0 +1,5 @@
+package srr.srr.Reservation;
+
+public class ReservationController {
+    
+}
---
+++ srr/src/test/java/srr/srr/SrrApplicationTests.java
@@ -0,0 +1,13 @@
+package srr.srr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
